40fb9dd9821046fe3e617e1ae71d5a6e5343f75d
anton2010000 <94874805+anton2010000@users.noreply.github.com>
2023-04-26 20:39:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <div id="main">
+        <h1>Онлайн магазин</h1>
+        <img src="https://i.pinimg.com/736x/7a/17/84/7a178452263eaf86d008d189a78b313f.jpg">
+        <button id="buy">Купить</button>
+    </div>
+</body>
+</html>
